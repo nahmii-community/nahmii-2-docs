80cee329cd97a00e37fea284b5c3227d2528ee41
Jens Ivar Jordre <jensivar@nahmii.io>
2021-10-26 23:13:28 +0200

    Update demo

--- l2-deposit-example/native-eth/index.js
+++ l2-deposit-example/native-eth/index.js
@@ -1,46 +1,84 @@
 const {
-  l1Wallet,
-  l2Wallet,
-  getAddressManager,
-  getGateway,
-  getNvmEth,
-  sleep
+    l1Wallet,
+    l2Wallet,
+    getL1Bridge,
+    getL2Bridge,
+    getNvmEth,
+    getL1FraudVerifier,
+    relayXDomainMessage,
+    initWatcher,
+    Direction,
+    waitXDomainTransaction,
+    sleep,
+    DEFAULT_TEST_GAS_L1,
+    DEFAULT_TEST_GAS_L2,
 } = require('./utils')
 
 const ethers = require('ethers')
 
 const run = async () => {
-  const addressManager = getAddressManager(l1Wallet)
-  const gateway = await getGateway(l1Wallet, addressManager)
+    const l1Bridge = await getL1Bridge()
+    const l2Bridge = await getL2Bridge()
+    const nvmEth = getNvmEth()
+    const l1FraudVerifier = await getL1FraudVerifier();
+    const watcher = await initWatcher();
 
-  const amount = ethers.utils.parseEther('0.01')
+    // Deposit into Nahmii
 
-  console.log(`L2 balance(${l2Wallet.address}) before deposit: ${(await l2Wallet.getBalance()).toString()}`)
-  const depositTx = await gateway.deposit({
-    value: amount
-  })
-  console.log(`deposit tx hash by wallet ${l2Wallet.address}: ${depositTx.hash}`)
+    const l2BlockNumber = await l2Wallet.provider.getBlockNumber()
 
-  await sleep(10000)
-  console.log(`L2 balance(${l2Wallet.address}) after deposit: ${(await l2Wallet.getBalance()).toString()}`)
+    console.log(`L2 balance(${l2Wallet.address}) before deposit: ${(await l2Wallet.getBalance()).toString()}`)
 
-  const nvmEth = getNvmEth(l2Wallet)
+    const depositAmount = ethers.utils.parseEther('0.01')
+    const depositTx = await l1Bridge.depositETH(
+        DEFAULT_TEST_GAS_L2,
+        '0xFFFF',
+        {
+            value: depositAmount,
+            gasLimit: DEFAULT_TEST_GAS_L1
+        }
+    )
+    console.log(`L1 deposit tx hash by wallet ${l1Wallet.address}: ${depositTx.hash}`)
+    await waitXDomainTransaction(watcher, depositTx, Direction.L1ToL2, l2BlockNumber)
+    console.log(`L2 balance(${l2Wallet.address}) after deposit: ${(await l2Wallet.getBalance()).toString()}`)
 
-  const other = ethers.Wallet.createRandom().connect(l2Wallet.provider)
-  const transferTx = await nvmEth.transfer(other.address, amount)
-  console.log(`L2 transfer tx hash by wallet ${l2Wallet.address}: ${transferTx.hash}`)
+    // Transfer in Nahmii
 
-  console.log(`L2 balance(${other.address}) after transfer: ${(await other.getBalance()).toString()}`)
+    const otherWallet = ethers.Wallet.createRandom().connect(l2Wallet.provider)
+    const transferAmount = ethers.utils.parseEther('0.001')
 
-  const withdrawnAmount = ethers.utils.parseEther('0.0095')
-  nvmEth.connect(other).withdraw(withdrawnAmount)
+    const transferTx = await nvmEth.transfer(
+        otherWallet.address,
+        transferAmount
+    )
+    console.log(`L2 transfer tx hash by wallet ${l2Wallet.address}: ${transferTx.hash}`)
+    await transferTx.wait()
+    console.log(`L2 balance(${otherWallet.address}) after transfer: ${(await otherWallet.getBalance()).toString()}`)
 
-  await sleep(10000)
+    // Withdraw from Nahmii
 
-  const l1BalanceAfter = await other
-      .connect(l1Wallet.provider)
-      .getBalance()
-  console.log(`L1 balance(${other.address}) after withdrawal: ${l1BalanceAfter.toString()}`)
+    const l1BlockNumber = await l1Wallet.provider.getBlockNumber()
+
+    const withdrawAmount = ethers.utils.parseEther('0.005')
+    const withdrawTx = await l2Bridge.withdraw(
+        nvmEth.address,
+        withdrawAmount,
+        DEFAULT_TEST_GAS_L2,
+        '0xFFFF'
+    )
+    console.log(`L2 withdraw tx hash by wallet ${l2Wallet.address}: ${withdrawTx.hash}`)
+    await withdrawTx.wait()
+
+    const fraudProofWindow = await l1FraudVerifier.FRAUD_PROOF_WINDOW()
+
+    console.log(`Sleeping for the duration of the fraud proof time window, i.e. ${fraudProofWindow.toString()}s`)
+    await sleep(fraudProofWindow.toNumber() * 1000)
+
+    await relayXDomainMessage(withdrawTx)
+    await waitXDomainTransaction(watcher, withdrawTx, Direction.L2ToL1, l1BlockNumber)
+
+    const l1BalanceAfter = await l1Wallet.getBalance()
+    console.log(`L1 balance(${l1Wallet.address}) after withdrawal: ${l1BalanceAfter.toString()}`)
 }
 
 run()
--- l2-deposit-example/native-eth/utils.js
+++ l2-deposit-example/native-eth/utils.js
@@ -1,35 +1,33 @@
 const {
-  getContractFactory,
-  getContractInterface,
+    getContractFactory,
+    getContractInterface,
 } = require('@nahmii/contracts')
-const { remove0x, Watcher } = require('@nahmii/core-utils')
-const {
-  Contract,
-  Wallet,
-  constants,
-  providers,
-  BigNumberish,
-  BigNumber,
-  utils,
-} = require('ethers')
-const { cleanEnv, str, num } = require('envalid')
+const {remove0x, Watcher} = require('@nahmii/core-utils')
+const {relayXDomainMessages} = require('@nahmii/message-relayer')
 
-module.exports.GWEI = BigNumber.from(1e9)
+const {
+    Contract,
+    Wallet,
+    constants,
+    providers,
+    utils,
+} = require('ethers')
+const {cleanEnv, str, num} = require('envalid')
 
 const env = cleanEnv(process.env, {
-  L1_URL: str({ default: 'https://l1.testnet.nahmii.io/' }),
-  L2_URL: str({ default: 'https://l2.testnet.nahmii.io/' }),
-  VERIFIER_URL: str({ default: 'http://localhost:8547' }),
-  L1_POLLING_INTERVAL: num({ default: 10 }),
-  L2_POLLING_INTERVAL: num({ default: 10 }),
-  VERIFIER_POLLING_INTERVAL: num({ default: 10 }),
-  PRIVATE_KEY: str({
-    default:
-      '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
-  }),
-  ADDRESS_MANAGER: str({
-    default: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-  }),
+    L1_URL: str({default: 'https://l1.testnet.nahmii.io/'}), // An Ethereum client on the Ropsten testnet
+    L2_URL: str({default: 'https://l2.testnet.nahmii.io/'}),
+    // VERIFIER_URL: str({ default: 'http://localhost:8547' }),
+    L1_POLLING_INTERVAL: num({default: 10}),
+    L2_POLLING_INTERVAL: num({default: 10}),
+    VERIFIER_POLLING_INTERVAL: num({default: 10}),
+    PRIVATE_KEY: str({
+        default:
+            '0x50df7674bda072b62550dc9e87ee7c137f4c994cad8b58c56ca652713fee8ea7',
+    }),
+    ADDRESS_MANAGER: str({
+        default: '0x357eCd03C1601273094Db3474A13D2452cF6785A',
+    }),
 })
 
 // The hardhat instance
@@ -41,9 +39,9 @@ const l2Provider = new providers.JsonRpcProvider(env.L2_URL)
 l2Provider.pollingInterval = env.L2_POLLING_INTERVAL
 module.exports.l2Provider = l2Provider
 
-const verifierProvider = new providers.JsonRpcProvider(env.VERIFIER_URL)
-verifierProvider.pollingInterval = env.VERIFIER_POLLING_INTERVAL
-module.exports.verifierProvider = verifierProvider
+// const verifierProvider = new providers.JsonRpcProvider(env.VERIFIER_URL)
+// verifierProvider.pollingInterval = env.VERIFIER_POLLING_INTERVAL
+// module.exports.verifierProvider = verifierProvider
 
 // The sequencer private key which is funded on L1
 const l1Wallet = new Wallet(env.PRIVATE_KEY, l1Provider)
@@ -51,54 +49,154 @@ module.exports.l1Wallet = l1Wallet
 
 // A random private key which should always be funded with deposits = require(L1 -> L2)
 // if it's using non-0 gas price
-module.exports.l2Wallet = l1Wallet.connect(l2Provider)
+const l2Wallet = l1Wallet.connect(l2Provider)
+module.exports.l2Wallet = l2Wallet
+
+// Default gas limits
+module.exports.DEFAULT_TEST_GAS_L1 = 330_000
+module.exports.DEFAULT_TEST_GAS_L2 = 1_300_000
 
 // Predeploys
-module.exports.PROXY_SEQUENCER_ENTRYPOINT_ADDRESS =
-  '0x4200000000000000000000000000000000000004'
-
 const NVM_ETH_ADDRESS = '0x4200000000000000000000000000000000000006'
 module.exports.NVM_ETH_ADDRESS = NVM_ETH_ADDRESS
 
-module.exports.getAddressManager = (provider) => {
-  return getContractFactory('Lib_AddressManager')
-    .connect(provider)
+const L2_STANDARD_BRIDGE_ADDRESS = '0x4200000000000000000000000000000000000010'
+module.exports.L2_STANDARD_BRIDGE_ADDRESS = L2_STANDARD_BRIDGE_ADDRESS
+
+const l1AddressManager = getContractFactory('Lib_AddressManager', l1Wallet)
     .attach(env.ADDRESS_MANAGER)
+
+// Gets the L1 bridge contract
+module.exports.getL1Bridge = async () => {
+    const proxyBridgeAddress = await l1AddressManager.getAddress(
+        'Proxy__NVM_L1StandardBridge'
+    )
+
+    const addressToUse =
+        proxyBridgeAddress !== constants.AddressZero
+            ? proxyBridgeAddress
+            : await l1AddressManager.getAddress('NVM_L1StandardBridge')
+
+    return new Contract(
+        addressToUse,
+        getContractInterface('NVM_L1StandardBridge'),
+        l1Wallet
+    )
 }
 
-// Gets the gateway using the proxy if available
-module.exports.getGateway = async (wallet, AddressManager) => {
-  const l1GatewayInterface = getContractInterface('NVM_L1ETHGateway')
-  const ProxyGatewayAddress = await AddressManager.getAddress(
-    'Proxy__NVM_L1ETHGateway'
-  )
-  const addressToUse =
-    ProxyGatewayAddress !== constants.AddressZero
-      ? ProxyGatewayAddress
-      : await AddressManager.getAddress('NVM_L1ETHGateway')
-
-  const NVM_L1ETHGateway = new Contract(
-    addressToUse,
-    l1GatewayInterface,
-    wallet
-  )
-
-  return NVM_L1ETHGateway
+// Gets the L2 bridge contract
+module.exports.getL2Bridge = async () => {
+    return new Contract(
+        L2_STANDARD_BRIDGE_ADDRESS,
+        getContractInterface('NVM_L2StandardBridge'),
+        l2Wallet
+    )
 }
 
-module.exports.getNvmEth = (wallet) => {
-  const NVM_ETH = new Contract(
-    NVM_ETH_ADDRESS,
-    getContractInterface('NVM_ETH'),
-    wallet
-  )
+// Gets the L2 ETH contract
+module.exports.getNvmEth = () => {
+    return new Contract(
+        NVM_ETH_ADDRESS,
+        getContractInterface('NVM_ETH'),
+        l2Wallet
+    )
+}
 
-  return NVM_ETH
+// Gets the L1 fraud verifier contract
+module.exports.getL1FraudVerifier = async () => {
+    const l1FraudVerifierAddress = await l1AddressManager.getAddress(
+        'NVM_FraudVerifier'
+    )
+
+    return new Contract(
+        l1FraudVerifierAddress,
+        getContractInterface('NVM_FraudVerifier'),
+        l1Provider
+    )
+}
+
+// Relays all L2 => L1 messages found in a given L2 transaction.
+module.exports.relayXDomainMessage = async (tx) => {
+    const l1MessengerAddress = await l1AddressManager.getAddress(
+        'Proxy__NVM_L1CrossDomainMessenger'
+    )
+
+    tx = await tx
+
+    return relayXDomainMessages(
+        tx.hash,
+        l1MessengerAddress,
+        l1Provider,
+        l2Provider,
+        l1Wallet
+    )
+}
+
+// Initializes a watcher instance
+module.exports.initWatcher = async () => {
+    const l1MessengerAddress = await l1AddressManager.getAddress(
+        'Proxy__NVM_L1CrossDomainMessenger'
+    )
+    const l2MessengerAddress = await l1AddressManager.getAddress(
+        'NVM_L2CrossDomainMessenger'
+    )
+    return new Watcher({
+        l1: {
+            provider: l1Provider,
+            messengerAddress: l1MessengerAddress,
+        },
+        l2: {
+            provider: l2Provider,
+            messengerAddress: l2MessengerAddress,
+        }
+    })
+}
+
+const Direction = Object.freeze({
+    L1ToL2: 0,
+    L2ToL1: 1
+})
+module.exports.Direction = Direction
+
+// Waits for cross-domain transaction
+module.exports.waitXDomainTransaction = async (watcher, tx, direction, fromBlock = 0) => {
+    const {src, dest}
+        = direction === Direction.L1ToL2
+        ? {src: watcher.l1, dest: watcher.l2}
+        : {src: watcher.l2, dest: watcher.l1}
+
+    // Await it if needed
+    tx = await tx
+
+    // Get the receipt and the full transaction
+    const receipt = await tx.wait()
+    const fullTx = await src.provider.getTransaction(tx.hash)
+
+    // Get the message hash which was created on the SentMessage
+    const [xDomainMsgHash] = await watcher.getMessageHashesFromTx(src, tx.hash)
+
+    // Get the transaction and receipt on the remote layer
+    const remoteReceipt = await watcher.getTransactionReceipt(
+        dest,
+        xDomainMsgHash,
+        true,
+        fromBlock
+    )
+    const remoteTx = await dest.provider.getTransaction(
+        remoteReceipt.transactionHash
+    )
+
+    return {
+        tx: fullTx,
+        receipt,
+        remoteTx,
+        remoteReceipt,
+    }
 }
 
 module.exports.sleep = (ms) => new Promise((r) => setTimeout(r, ms))
 
 const abiCoder = new utils.AbiCoder()
 module.exports.encodeSolidityRevertMessage = (_reason) => {
-  return '0x08c379a0' + remove0x(abiCoder.encode(['string'], [_reason]))
+    return '0x08c379a0' + remove0x(abiCoder.encode(['string'], [_reason]))
 }
